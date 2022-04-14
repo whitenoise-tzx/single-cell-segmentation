3662713b1469f1e98301abdebc5da1a99d6bb016
whitenoise-tzx <72962618+whitenoise-tzx@users.noreply.github.com>
2022-04-15 00:16:51 +0800

    Add files via upload

--- dataset.py
+++ dataset.py
@@ -1,3 +1,4 @@
+import torch
 import torch.utils.data as data
 import PIL.Image as Image
 from sklearn.model_selection import train_test_split
@@ -7,7 +8,7 @@ import numpy as np
 import skimage
 import cv2
 from glob import glob
-import imageio
+from skimage import io
 from skimage.io import imread
 import helpers
 
@@ -87,10 +88,10 @@ class NMuMgDataset(data.Dataset):
 
     def getDataPath(self):
 
-        self.img_paths = glob(self.root + 'train/data/*')
-        self.mask_paths = glob(self.root + 'train/label/*')
-        self.test_img_paths = glob(self.root + 'test/data/*')
-        self.test_mask_paths = glob(self.root + 'test/label/*')
+        self.img_paths = glob(self.root + 'train/Img/*')
+        self.mask_paths = glob(self.root + 'train/BIB/*')
+        self.test_img_paths = glob(self.root + 'test/enh_data/*')
+        self.test_mask_paths = glob(self.root + 'test/BIB/*')
         # self.val_img_paths = glob(self.root + r'\val\val_images\*')
         # self.val_mask_paths = glob(self.root + r'\val\val_mask\*')
         self.train_img_paths, self.val_img_paths, self.train_mask_paths, self.val_mask_paths = \
@@ -109,14 +110,29 @@ class NMuMgDataset(data.Dataset):
         mask_path = self.masks[index]
         # origin_x = Image.open(x_path)
         # origin_y = Image.open(y_path)
-        pic = Image.open(pic_path)
-        pic = np.array(pic)
-        pic = np.expand_dims(pic, axis=2)
+        '''
+        #pic = Image.open(pic_path)
+        #pic = np.array(pic)
+        #pic = np.expand_dims(pic, axis=2)
+        pic= cv2.imread(pic_path)
         pic = pic.astype('float32')
         mask = Image.open(mask_path)
         mask = np.array(mask)
         mask = np.expand_dims(mask, axis=2)
         mask = helpers.mask_to_onehot(mask, palette)
+        '''
+        pic = Image.open(pic_path)
+        pic = Image.fromarray(np.uint8(pic))
+        pic = pic.convert('L')
+        pic = np.array(pic, dtype='float32')
+        pic = np.expand_dims(pic, axis=2)
+        mask = Image.open(mask_path)
+        mask = Image.fromarray(np.uint8(mask))
+        mask = np.asarray(mask)
+        mask = np.expand_dims(mask, axis=2)
+        mask = helpers.mask_to_onehot(mask, palette)
+        pic = pic.astype('float32')  # / 255
+        mask = mask.astype('float32')  # / 255
         # if self.aug:
         #     if random.uniform(0, 1) > 0.5:
         #         pic = pic[:, ::-1, :].copy()
@@ -146,14 +162,84 @@ class T47DDataset(data.Dataset):
         self.pics,self.masks = self.getDataPath()
         self.transform = transform
         self.target_transform = target_transform
-
     def getDataPath(self):
-
         self.img_paths = glob(self.root + r'/train/data/*')
         self.mask_paths = glob(self.root + r'/train/label/*')
         self.test_img_paths = glob(self.root + r'/test/data/*')
         self.test_mask_paths = glob(self.root + r'/test/label/*')
+        # self.val_img_paths = glob(self.root + r'\val\val_images\*')
+        # self.val_mask_paths = glob(self.root + r'\val\val_mask\*')
+        self.train_img_paths, self.val_img_paths, self.train_mask_paths, self.val_mask_paths = \
+            train_test_split((self.img_paths), (self.mask_paths), test_size=0.2, random_state=42)
+        # self.test_img_paths, self.test_mask_paths = self.val_img_paths,self.val_mask_paths
+        assert self.state == 'train' or self.state == 'val' or self.state == 'test'
+        if self.state == 'train':
+            return (self.train_img_paths), (self.train_mask_paths)
+        if self.state == 'val':
+            return (self.val_img_paths), (self.val_mask_paths)
+        if self.state == 'test':
+            return (self.test_img_paths), (self.test_mask_paths)
 
+    def __getitem__(self, index):
+        pic_path = self.pics[index]
+        mask_path = self.masks[index]
+        # origin_x = Image.open(x_path)
+        # origin_y = Image.open(y_path)
+        '''
+        #pic = Image.open(pic_path)
+        #pic = np.array(pic)
+        #pic = np.expand_dims(pic, axis=2)
+        pic = cv2.imread(pic_path)
+        pic = pic.astype('float32')
+        mask = Image.open(mask_path)
+        mask = np.array(mask)
+        mask = np.expand_dims(mask, axis=2)
+        mask = helpers.mask_to_onehot(mask, palette)
+        '''
+        pic = Image.open(pic_path)
+        pic = Image.fromarray(np.uint8(pic))
+        pic = pic.convert('L')
+        pic = np.array(pic, dtype='float32')
+        pic = np.expand_dims(pic, axis=2)
+        mask = Image.open(mask_path)
+        mask = Image.fromarray(np.uint8(mask))
+        mask = np.asarray(mask)
+        mask = np.expand_dims(mask, axis=2)
+        mask = helpers.mask_to_onehot(mask, palette)
+        pic = pic.astype('float32')  # / 255
+        mask = mask.astype('float32')  # / 255
+        # if self.aug:
+        #     if random.uniform(0, 1) > 0.5:
+        #         pic = pic[:, ::-1, :].copy()
+        #         mask = mask[:, ::-1].copy()
+        #     if random.uniform(0, 1) > 0.5:
+        #         pic = pic[::-1, :, :].copy()
+        #         mask = mask[::-1, :].copy()
+        if self.transform is not None:
+            img_x = self.transform(pic)
+        if self.target_transform is not None:
+            img_y = self.target_transform(mask)
+        return img_x, img_y,pic_path,mask_path
+    def __len__(self):
+        return len(self.pics)
+
+class HK2_DICDataset(data.Dataset):
+    def __init__(self, state, transform=None, target_transform=None):
+        self.state = state
+        self.aug = True
+        self.root = '/shared/home/v_zixin_tang/dataset/HK2_DIC/'
+        self.img_paths = None
+        self.mask_paths = None
+        self.train_img_paths, self.val_img_paths,self.test_img_paths = None,None,None
+        self.train_mask_paths, self.val_mask_paths,self.test_mask_paths = None,None,None
+        self.pics,self.masks = self.getDataPath()
+        self.transform = transform
+        self.target_transform = target_transform
+    def getDataPath(self):
+        self.img_paths = glob(self.root + 'train/transfer/*')
+        self.mask_paths = glob(self.root + 'train/enh_BIB/*')
+        self.test_img_paths = glob(self.root + 'test/transfer/*')
+        self.test_mask_paths = glob(self.root + 'test/BIB/*')
         # self.val_img_paths = glob(self.root + r'\val\val_images\*')
         # self.val_mask_paths = glob(self.root + r'\val\val_mask\*')
         self.train_img_paths, self.val_img_paths, self.train_mask_paths, self.val_mask_paths = \
@@ -173,25 +259,43 @@ class T47DDataset(data.Dataset):
         # origin_x = Image.open(x_path)
         # origin_y = Image.open(y_path)
         """
-        pic = np.array(imread(pic_path), dtype=np.float64)
-        cv2.normalize(pic, pic, 0, 255, cv2.NORM_MINMAX)
-        pic = cv2.convertScaleAbs(pic)
-        pic = np.array(pic)
+        pic = Image.open(pic_path)
+        pic = Image.fromarray(np.uint8(pic))
+        pic = pic.convert('L')
+        pic = np.array(pic, dtype='float32')
+        pic = np.expand_dims(pic, axis=2)
+        mask = Image.open(mask_path)
+        mask = Image.fromarray(np.uint8(mask))
+        mask = np.asarray(mask)
+        mask = np.expand_dims(mask, axis=2)
+        mask = helpers.mask_to_onehot(mask, palette)
         pic = pic.astype('float32')  # / 255
+        mask = mask.astype('float32')  # / 255
+        """
+        img = Image.open(pic_path)
+        img = np.array(img)
+        img = img.astype('float32')
+        #img = np.array(img, dtype='float32')
         mask = Image.open(mask_path)
         mask = Image.fromarray(np.uint8(mask))
-        mask = mask.convert("L")
-        mask = np.expand_dims(mask, axis=2)
-        mask = helpers.mask_to_onehot(mask, palette)
-        """
-        pic = Image.open(pic_path)
-        pic = np.array(pic)
-        pic = np.expand_dims(pic, axis=2)
-        pic = pic.astype('float32')
-        mask = Image.open(mask_path)
         mask = np.array(mask)
-        mask = np.expand_dims(mask, axis=2)
-        mask = helpers.mask_to_onehot(mask, palette)
+        seed=np.random.randint(0,2**32)
+        random.seed(seed)
+        torch.manual_seed(seed)
+        img = (img - np.amin(img)) * 1.0 / (np.amax(img) - np.amin(img))
+        img = img * 1.0 / np.median(img)
+        img_h = img.shape[0]
+        img_w = img.shape[1]
+        img_x = np.reshape(img, (1,img_h, img_w))
+        mask_h = mask.shape[0]
+        mask_w = mask.shape[1]
+        label_map=np.zeros([mask_h,mask_w,3])
+        for r in range(mask_h):
+            for c in range(mask_w):
+                label_map[r, c, mask[r][c]] = 1
+        random.seed(seed)
+        torch.manual_seed(seed)
+        img_y=np.transpose(label_map,(2,0,1))
         # if self.aug:
         #     if random.uniform(0, 1) > 0.5:
         #         pic = pic[:, ::-1, :].copy()
@@ -199,85 +303,14 @@ class T47DDataset(data.Dataset):
         #     if random.uniform(0, 1) > 0.5:
         #         pic = pic[::-1, :, :].copy()
         #         mask = mask[::-1, :].copy()
+        '''
         if self.transform is not None:
             img_x = self.transform(pic)
         if self.target_transform is not None:
             img_y = self.target_transform(mask)
+        '''
         return img_x, img_y,pic_path,mask_path
-
     def __len__(self):
         return len(self.pics)
 
-class HK2_DICDataset(data.Dataset):
-    def __init__(self, state, transform=None, target_transform=None):
-        self.state = state
-        self.aug = True
-        self.root = '/shared/home/v_zixin_tang/dataset/HK2_DIC/'
-        self.img_paths = None
-        self.mask_paths = None
-        self.train_img_paths, self.val_img_paths,self.test_img_paths = None,None,None
-        self.train_mask_paths, self.val_mask_paths,self.test_mask_paths = None,None,None
-        self.pics,self.masks = self.getDataPath()
-        self.transform = transform
-        self.target_transform = target_transform
 
-    def getDataPath(self):
-
-        self.img_paths = glob(self.root + 'train/data/*')
-        self.mask_paths = glob(self.root + 'train/label/*')
-        self.test_img_paths = glob(self.root + 'test/data/*')
-        self.test_mask_paths = glob(self.root + 'test/label/*')
-        # self.val_img_paths = glob(self.root + r'\val\val_images\*')
-        # self.val_mask_paths = glob(self.root + r'\val\val_mask\*')
-        self.train_img_paths, self.val_img_paths, self.train_mask_paths, self.val_mask_paths = \
-            train_test_split((self.img_paths), (self.mask_paths), test_size=0.2, random_state=42)
-        # self.test_img_paths, self.test_mask_paths = self.val_img_paths,self.val_mask_paths
-        assert self.state == 'train' or self.state == 'val' or self.state == 'test'
-        if self.state == 'train':
-            return (self.train_img_paths), (self.train_mask_paths)
-        if self.state == 'val':
-            return (self.val_img_paths), (self.val_mask_paths)
-        if self.state == 'test':
-            return (self.test_img_paths), (self.test_mask_paths)
-
-    def __getitem__(self, index):
-        pic_path = self.pics[index]
-        mask_path = self.masks[index]
-        # origin_x = Image.open(x_path)
-        # origin_y = Image.open(y_path)
-        pic = Image.open(pic_path)
-        pic = np.array(pic)
-        pic = np.expand_dims(pic, axis=2)
-        pic = pic.astype('float32')
-        mask = Image.open(mask_path)
-        mask = np.array(mask)
-        mask = np.expand_dims(mask, axis=2)
-        mask = helpers.mask_to_onehot(mask, palette)
-        """
-        pic = np.array(imread(pic_path), dtype=np.float64)
-        cv2.normalize(pic, pic, 0, 255, cv2.NORM_MINMAX)
-        pic = cv2.convertScaleAbs(pic)
-        pic = np.array(pic)
-        pic = np.expand_dims(pic, axis=2)
-        pic = pic.astype('float32') #/ 255
-        mask = Image.open(mask_path)
-        mask = Image.fromarray(np.uint8(mask))
-        mask = mask.convert("L")
-        mask = np.expand_dims(mask, axis=2)
-        mask = helpers.mask_to_onehot(mask, palette)
-        """
-        # if self.aug:
-        #     if random.uniform(0, 1) > 0.5:
-        #         pic = pic[:, ::-1, :].copy()
-        #         mask = mask[:, ::-1].copy()
-        #     if random.uniform(0, 1) > 0.5:
-        #         pic = pic[::-1, :, :].copy()
-        #         mask = mask[::-1, :].copy()
-        if self.transform is not None:
-            img_x = self.transform(pic)
-        if self.target_transform is not None:
-            img_y = self.target_transform(mask)
-        return img_x, img_y,pic_path,mask_path
-
-    def __len__(self):
-        return len(self.pics)
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from r2unet import R2U_Net
 from segnet import SegNet
 from unetpp import NestedUNet
 from fcn import get_fcn8s
+from U_Net import U_Net
 from dataset import *
 from metrics import *
 from metrics_3 import *
@@ -22,16 +23,16 @@ from plot import loss_plot
 from plot import metrics_plot
 from torchvision.models import vgg16
 import os
-os.environ['CUDA_VISIBLE_DEVICES']='1'
+
 def getArgs():
     parse = argparse.ArgumentParser()
     parse.add_argument('--deepsupervision', default=0)
-    parse.add_argument("--action", type=str, help="train/test/train&test", default="train&test")
+    parse.add_argument("--action", type=str, help="train/test/train&test", default="test")
     parse.add_argument("--epoch", type=int, default=41)
-    parse.add_argument('--arch', '-a', metavar='ARCH', default='r2unet',
+    parse.add_argument('--arch', '-a', metavar='ARCH', default='unet++',
                        help='UNet/resnet34_unet/unet++/myChannelUnet/Attention_UNet/segnet/r2unet/fcn32s/fcn8s')
-    parse.add_argument("--batch_size", type=int, default=1)
-    parse.add_argument('--dataset', default='T47D',  # dsb2018_256
+    parse.add_argument("--batch_size", type=int, default=4)
+    parse.add_argument('--dataset', default='HK2_DIC',
                        help='dataset name:isbiCell/NMuMg/T47D/HK2_DIC')
     # parse.add_argument("--ckp", type=str, help="the path of model weight file")
     parse.add_argument("--log_dir", default='result/log', help="log dir")
@@ -54,9 +55,10 @@ def getLog(args):
 
 def getModel(args):
     if args.arch == 'UNet':
-        model = Unet(1, 3).to(device)
+        #model = Unet(1, 3).to(device)
+        model = U_Net(1, 3).to(device)
     if args.arch == 'resnet34_unet':
-        model = resnet34_unet(pretrained=False).to(device)
+        model = resnet34_unet(pretrained=True).to(device)
     if args.arch == 'unet++':
         args.deepsupervision = True
         model = NestedUNet(args,1,3).to(device)
@@ -65,13 +67,12 @@ def getModel(args):
     if args.arch == 'segnet':
         model = SegNet(1,3).to(device)
     if args.arch == 'r2unet':
-        model = R2U_Net(1,3).to(device)
+        model = R2U_Net(3,3).to(device)
     #if args.arch == 'fcn32s':
          #model = get_fcn32s(3).to(device)
     if args.arch == 'myChannelUnet':
         model = myChannelUnet(1,3).to(device)
     if args.arch == 'fcn8s':
-        assert args.dataset !='esophagus' ,"fcn8s模型不能用于数据集esophagus，因为esophagus数据集为80x80，经过5次的2倍降采样后剩下2.5x2.5，分辨率不能为小数，建议把数据集resize成更高的分辨率再用于fcn"
         model = get_fcn8s(3).to(device)
     if args.arch == 'cenet':
         from cenet import CE_Net_
@@ -102,11 +103,11 @@ def getDataset(args):
         test_dataset = T47DDataset(r"test", transform=x_transforms, target_transform=y_transforms)
         test_dataloaders = DataLoader(test_dataset, batch_size=1)
     if args.dataset == 'HK2_DIC':
-        train_dataset = HK2_DICDataset(r'train', transform=x_transforms, target_transform=y_transforms)
+        train_dataset = HK2_DICDataset(r'train',transform=x_transforms, target_transform=y_transforms)
         train_dataloaders = DataLoader(train_dataset, batch_size=args.batch_size)
-        val_dataset = HK2_DICDataset(r"val", transform=x_transforms, target_transform=y_transforms)
+        val_dataset = HK2_DICDataset(r"val",transform=x_transforms, target_transform=y_transforms)
         val_dataloaders = DataLoader(val_dataset, batch_size=1)
-        test_dataset = HK2_DICDataset(r"test", transform=x_transforms, target_transform=y_transforms)
+        test_dataset = HK2_DICDataset(r"test",transform=x_transforms, target_transform=y_transforms)
         test_dataloaders = DataLoader(test_dataset, batch_size=1)
     return train_dataloaders,val_dataloaders,test_dataloaders
 
@@ -116,7 +117,8 @@ def val(model,best_iou,val_dataloaders):
         i=0   #验证集中第i张图
         miou_total = 0
         pixel_accuracy_total= 0
-        #CCA_total=0
+        boundary_iou_total = 0
+        avgHausdorff_total=0
         num = len(val_dataloaders)  #验证集图片的总数
         print('验证集图片总数:',num)
         for x, _,pic,mask in val_dataloaders:
@@ -127,19 +129,29 @@ def val(model,best_iou,val_dataloaders):
             else:
                 img_y = torch.squeeze(y).cpu().numpy()  #输入损失函数之前要把预测图变成numpy格式，且为了跟训练图对应，要额外加多一维表示batchsize
             img_gt = Image.open(mask[0])
+            img_gt = Image.fromarray(np.uint8(img_gt))
             img_gt = np.asarray(img_gt)
             pred=np.argmax(img_y,axis=0)
+            '''
+            plt.figure()
+            plt.subplot(1, 3, 1)
+            plt.imshow(img_gt)
+            plt.subplot(1, 3, 2)
+            plt.imshow(pred)
+            plt.subplot(1, 3, 3)
+            plt.imshow(torch.squeeze(x).cpu().numpy())
+            plt.show()
+            '''
             pixel_accuracy_total+= pixel_accuracy(pred,img_gt)
             miou_total += mean_IU(pred,img_gt)
-            #CCA_total += accuracy_score(pred,img_gt)
             if i < num:i+=1   #处理验证集下一张图
         torch.save(model.state_dict(), r'./saved_model/' + str(args.arch) + '_' + str(args.batch_size) + '_' + str(
             args.dataset) + '_' + str(args.epoch) + '.pth')
         aver_pixel_accuracy=pixel_accuracy_total/num
         aver_iou = miou_total / num
-        #aver_CCA = CCA_total/num
         print('aver_pixel_accuracy=%f, Miou=%f' % (aver_pixel_accuracy,aver_iou))
         logging.info('aver_pixel_accuracy=%f, Miou=%f' % (aver_pixel_accuracy,aver_iou))
+
         if aver_iou > best_iou:
             print('aver_iou:{} > best_iou:{}'.format(aver_iou,best_iou))
             logging.info('aver_iou:{} > best_iou:{}'.format(aver_iou,best_iou))
@@ -157,7 +169,6 @@ def train(model, criterion, optimizer, train_dataloader,val_dataloader, args):
     loss_list = []
     iou_list = []
     pixel_accuracy_list = []
-    #CCA_list = []
     for epoch in range(num_epochs):
         model = model.train()
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
@@ -182,7 +193,6 @@ def train(model, criterion, optimizer, train_dataloader,val_dataloader, args):
             else:
                 output = model(inputs)
                 output = output.to(device)
-                #print(output.shape)
                 loss = criterion(output, torch.argmax(labels, dim=1))
             if threshold!=None:
                 if loss > threshold:
@@ -193,26 +203,24 @@ def train(model, criterion, optimizer, train_dataloader,val_dataloader, args):
                 loss.backward()
                 optimizer.step()
                 epoch_loss += loss.item() #item()取标量tensor
-
             print("%d/%d,train_loss:%0.3f" % (step, (dt_size - 1) // train_dataloader.batch_size + 1, loss.item()))
             logging.info("%d/%d,train_loss:%0.3f" % (step, (dt_size - 1) // train_dataloader.batch_size + 1, loss.item()))
         loss_list.append(epoch_loss)
         best_iou,aver_iou,aver_pixel_accuracy = val(model,best_iou,val_dataloader)
         iou_list.append(aver_iou)
         pixel_accuracy_list.append(aver_pixel_accuracy)
-        #CCA_list.append(aver_CCA)
         print("epoch %d loss:%0.3f" % (epoch, epoch_loss))
         logging.info("epoch %d loss:%0.3f" % (epoch, epoch_loss))
     loss_plot(args, loss_list)
     metrics_plot(args, 'iou',iou_list)
     metrics_plot(args,'pixel accuracy',pixel_accuracy_list)
-    #metrics_plot(args, 'CCA', CCA_list)
     return model
 
 def test(val_dataloaders,save_predict=False):
     logging.info('final test........')
     if save_predict ==True:
-        dir = os.path.join(r'./saved_predict',str(args.arch),str(args.batch_size),str(args.epoch),str(args.dataset))
+        #dir = os.path.join(r'./saved_predict',str(args.arch),str(args.batch_size),str(args.epoch),str(args.dataset))
+        dir = os.path.join(r'./saved_predict', 'transfer',str(args.arch), str(args.batch_size), str(args.epoch), str(args.dataset))
         if not os.path.exists(dir):
             os.makedirs(dir)
         else:
@@ -226,7 +234,8 @@ def test(val_dataloaders,save_predict=False):
         i=0   #验证集中第i张图
         miou_total = 0
         pixel_accuracy_total = 0
-        #CCA_total = 0
+        boundary_iou_total = 0
+        avgHausdorff_total = 0
         num = len(val_dataloaders)  #验证集图片的总数
         for pic,_,pic_path,mask_path in val_dataloaders:
             pic = pic.to(device)
@@ -237,27 +246,28 @@ def test(val_dataloaders,save_predict=False):
                 predict = torch.squeeze(predict).cpu().numpy()  #输入损失函数之前要把预测图变成numpy格式，且为了跟训练图对应，要额外加多一维表示batchsize
             #img_y = torch.squeeze(y).cpu().numpy()  #输入损失函数之前要把预测图变成numpy格式，且为了跟训练图对应，要额外加多一维表示batchsize
             img_gt = Image.open(mask_path[0])
+            img_gt = Image.fromarray(np.uint8(img_gt))
             img_gt = np.asarray(img_gt)
             pred = np.argmax(predict, axis=0)
             pixel_acc=pixel_accuracy(pred, img_gt)
             pixel_accuracy_total += pixel_acc
             iou=mean_IU(pred, img_gt)
             miou_total += iou
-            #CCA_=accuracy_score(pred, img_gt)
-            #CCA_total += CCA_
+            boundary_iou_total += boundary_iou(img_gt, pred)
+            quality = computeQualityMeasures(pred, img_gt)
             #_, predicted = torch.max(torch.tensor(predict), dim=1)
             #total += mask_path[0].size(0)
             #correct += (predicted == mask_path[0]).sum().item()
             fig = plt.figure()
             ax1 = fig.add_subplot(1, 3, 1)
             ax1.set_title('input')
-            plt.imshow(Image.open(pic_path[0]), cmap='Greys_r')
+            plt.imshow(Image.open(pic_path[0]))
             ax2 = fig.add_subplot(1, 3, 2)
             ax2.set_title('predict')
-            plt.imshow(pred,cmap = plt.get_cmap('gray'))
+            plt.imshow(pred)
             ax3 = fig.add_subplot(1, 3, 3)
             ax3.set_title('mask')
-            plt.imshow(img_gt, cmap='Greys_r')
+            plt.imshow(img_gt)
             if save_predict == True:
                 if args.dataset == 'driveEye':
                     saved_predict = dir + '/' + mask_path[0].split('\\')[-1]
@@ -269,22 +279,36 @@ def test(val_dataloaders,save_predict=False):
             #plt.pause(0.01)
             print('iou={},pixel_accuracy={}'.format(iou,pixel_acc))
             if i < num:i+=1   #处理验证集下一张图
-        #plt.show()
+        plt.show()
+        aver_pixel_accuracy = pixel_accuracy_total / num
+        aver_iou = miou_total / num
+        aver_boundary_iou = boundary_iou_total / num
+        aver_avgHausdorff = avgHausdorff_total / num
         #print('accuracy on test set: %d %% ' % (100 * correct / total))
-        print('Miou=%f,aver_pixel_accuracy=%f' % (miou_total/num,pixel_accuracy_total/num))
-        logging.info('Miou=%f,aver_hd=%f' % (miou_total/num,pixel_accuracy_total/num))
+        print('aver_pixel_accuracy=%f, Miou=%f, aver_boundary_iou=%f, aver_avgHausdorff=%f' % (aver_pixel_accuracy, aver_iou, aver_boundary_iou, aver_avgHausdorff))
+        logging.info('aver_pixel_accuracy=%f, Miou=%f, aver_boundary_iou=%f, aver_avgHausdorff=%f' % (aver_pixel_accuracy, aver_iou, aver_boundary_iou, aver_avgHausdorff))
+
 
 if __name__ =="__main__":
     palette = [[0], [1], [2]]
+
+    norm_mean = [0.485, 0.456, 0.406]
+    norm_std = [0.229, 0.224, 0.225]
     x_transforms = transforms.Compose([
         transforms.ToTensor(),  # -> [0,1]
-        #transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])  # ->[-1,1]
-        transforms.Normalize((0.5,), (0.5,))
+        transforms.Normalize((0.5,), (0.5,)),
+        #transforms.Normalize(norm_mean, norm_std)
     ])
     # mask只需要转换为tensor
-    y_transforms = transforms.ToTensor()
+    y_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        #transforms.Resize((256, 256)),
+    ])
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = '5,6,7'
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
+
     args = getArgs()
     logging = getLog(args)
     print('**************************')
@@ -294,7 +318,7 @@ if __name__ =="__main__":
           (args.arch, args.epoch, args.batch_size,args.dataset))
     print('**************************')
     model = getModel(args)
-    #model.load_state_dict(torch.load('./saved_model/UNet_1_T47D_21.pth'))  # 再加载网络的参数
+    #model.load_state_dict(torch.load('./saved_model/unet++_4_HK2_DIC_41.pth'))  # 再加载网络的参数
     #model = model.to(device)
     #print("load success")
     train_dataloaders,val_dataloaders,test_dataloaders = getDataset(args)
